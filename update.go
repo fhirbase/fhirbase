6b125bb44c7f463ed04787b336e53b1c6eedf88b
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2018-09-30 14:58:37 -0400

    some work on update command

--- update.go
+++ update.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/blang/semver"
+	"github.com/pkg/errors"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 	"github.com/urfave/cli"
 )
@@ -13,12 +17,35 @@ const FhirbaseRepo = "fhirbase/fhirbase"
 
 func updateCommand(c *cli.Context) error {
 	latest, found, err := selfupdate.DetectLatest(FhirbaseRepo)
+
 	if err != nil {
-		log.Println("Error occurred while detecting version:", err)
-		return err
+		return errors.Wrap(err, "error finding most recent fhirbase release")
 	}
 
-	fmt.Printf("Latest fhirbase version %v %v:", found, latest)
+	currentVersion := semver.MustParse(Version)
+
+	if !found || latest.Version.Equals(currentVersion) {
+		log.Println("Current version is the latest.")
+		return nil
+	}
+
+	fmt.Print("Do you want to update Fhirbase to the version", latest.Version, "? (y/n): ")
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil || (input != "y\n" && input != "n\n") {
+		fmt.Printf("Invalid input. Only 'y' or 'n' are accepted.\n")
+		return nil
+	}
+
+	if input == "n\n" {
+		return nil
+	}
+
+	if err := selfupdate.UpdateTo(latest.AssetURL, os.Args[0]); err != nil {
+		return errors.Wrap(err, "Error occurred while updating Fhirbase binary")
+	}
+
+	fmt.Printf("Successfully updated Fhirbase to version %s\n", latest.Version)
 
 	return nil
 }
